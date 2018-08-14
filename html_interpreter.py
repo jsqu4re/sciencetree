14623d7bf5f4fd4df63c0a0ee75ec8f166ccecae
Johannes Jeising <Johannes.Jeising@gmail.com>
2018-08-14 04:00:03 -0700

    create recursive crawler

--- html_interpreter.py
+++ html_interpreter.py
@@ -10,5 +10,16 @@ tree = html.fromstring(html_file)
 search_results = tree.xpath("//div[contains(@class, 'col-22-24')]")
 # search_results = tree.xpath("//a[contains(@class, 'ng-binding ng-scope')]")
 
+titels = set()
+
 for result in search_results:
-    print result.getChildren()
+    for element in result.getchildren():
+        if element.tag == 'h2':
+            for text in element.getchildren():
+                if text.tag == 'a':
+                    titels.add(text.text_content())
+
+for titel in titels:
+    print titel
+
+print "Results: " + str(len(titels))
--- ieee-crawler-qt5.py
+++ ieee-crawler-qt5.py
@@ -10,56 +10,53 @@ from PyQt5.QtNetwork import *
 import sys
 from optparse import OptionParser
 
-from time import sleep
+from multiprocessing import Process, Queue
 
-class MyBrowser(QWebPage):
-    ''' Settings for the browser.'''
- 
-    def userAgentForUrl(self, url):
-        ''' Returns a User Agent that will be seen by the website. '''
-        return "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
- 
 class Browser(QWebView):
     def __init__(self, url):
         self.app = QApplication(sys.argv) 
         # QWebView
         self.view = QWebView.__init__(self)
-        #self.view.setPage(MyBrowser())
-        self.setWindowTitle('Loading...')
-        self.titleChanged.connect(self.adjustTitle)
-        self.loadFinished.connect(self._loadFinished)
-        #super(Browser).connect(self.ui.webView,QtCore.SIGNAL("titleChanged (const QString&)"), self.adjustTitle)
+        self.setWindowTitle('IEEE')
         self.load(url)
-        timer = QTimer(self)
-        timer.timeout.connect(self.do_quit)
-        timer.setInterval(20000)
-        timer.start()
+        self.timer = QTimer(self)
+        # self.timer.singleShot(20000, self.do_quit)
+        # self.timer = QTimer(self)
+        self.timer.timeout.connect(self.do_quit)
+        self.timer.setInterval(20000)
+        # self.timer.setSingleShot(True)
+        self.timer.start()
         self.app.exec_()
+        self.timer.stop()
     
     def do_quit(self):
         self.app.quit()
 
     def load(self,url):
         self.setUrl(QUrl(url))
- 
-    def adjustTitle(self):
-        self.setWindowTitle(self.title())
+
+def render(q, page_number):
+    file = open("ieee_download_" + str(page_number) + ".html", "w")
+    view = Browser("https://ieeexplore.ieee.org/search/searchresult.jsp?queryText=(%22motion%20planning%22)&matchBoolean=true&pageNumber=" + str(page_number) + "&rowsPerPage=10&searchField=Search_All")
+    file.write(view.page().mainFrame().toHtml().encode('ascii','ignore'))
+    q.put( "Created " + file.name )    
+
+
+
+if __name__ == '__main__':
+    print "start crawler"
     
-    def _loadFinished(self, result):
-        # self.frame = self.mainFrame()
-        # time.sleep(30)
-        print self.page().mainFrame().toHtml().encode('ascii','ignore')
-        # self.app.quit()
+    for number in range(1,658):
+        queue = Queue()
+        p = Process(target=render, args=(queue, number))
+        p.start()
+        p.join() # this blocks until the process terminates
+        result = queue.get()
+        print result
 
-    def disableJS(self):
-        settings = QWebSettings.globalSettings()
-        settings.setAttribute(QWebSettings.JavascriptEnabled, False)
+    print "done"
 
-view = Browser("https://ieeexplore.ieee.org/search/searchresult.jsp?queryText=(%22motion%20planning%22)&matchBoolean=true&pageNumber=2&rowsPerPage=10&searchField=Search_All")
-# print view.page().mainFrame().toHtml().encode('ascii','ignore')
+    # render(0)
 
-# view.showMaximized()
-# view.load("https://ieeexplore.ieee.org/search/searchresult.jsp?queryText=(%22motion%20planning%22)&matchBoolean=true&pageNumber=2&rowsPerPage=10&searchField=Search_All")
-# view.load("https://ieeexplore.ieee.org/search/searchresult.jsp?queryText=(%22motion%20planning%22)&matchBoolean=true&pageNumber=3&rowsPerPage=10&searchField=Search_All")
-# view.app.exec_()
-# print view.page().mainFrame().toHtml().encode('ascii','ignore')
+    # for page_number in range (0,3):
+    #     render(page_number)
